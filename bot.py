c9216073731331ba380c62848a08dbb46db2840f
smurfhrk1 <smurfhrk1@gmail.com>
2026-08-28 17:21:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,4 +96,4 @@ if __name__ == "__main__":
         c = i
         #print(f"Proses: {nama} ({email})")
         asyncio.run(main(nama, email, c))
-    print(f"Angka terakhir : {c}")
+    print("Selesai")
