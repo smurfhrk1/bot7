dc26ce6decdd09e4128cbbc0b7826b1d8cef427c
smurfhrk1 <smurfhrk1@gmail.com>
2026-09-04 08:47:30 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,6 +72,10 @@ async def main(nama, email, c):
         await page.wait_for_timeout(2000)
         await page.keyboard.press("Enter")
         await page.wait_for_timeout(2000)
+        await page.keyboard.press("Enter")
+        await page.wait_for_timeout(2000)
+        await page.mouse.click(170, 662)
+        await page.wait_for_timeout(2000)
         await page.mouse.click(170, 662)
         await page.wait_for_timeout(2000)
         if(c==0):
